e693f7ad234c0fa37363a409edaa86d9b31aaeab
mikebell <mikebell88008@gmail.com>
2024-12-15 12:17:34 -0500

    calculator grpc (Unary API) complete

---
+++ calculator/client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/mikebellcoder/udemy-grpc-master-class/calculator/proto"
+)
+
+var svrAddr = "localhost:5051"
+
+func main() {
+	conn, err := grpc.NewClient(svrAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connec: %v\n", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewCalculatorServiceClient(conn)
+
+	doSum(c)
+}
---
+++ calculator/client/sum.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/mikebellcoder/udemy-grpc-master-class/calculator/proto"
+)
+
+func doSum(c pb.CalculatorServiceClient) {
+	log.Println("doSum was invoked")
+
+	res, err := c.Sum(context.Background(), &pb.SumRequest{
+		FirstNumber:  3,
+		SecondNumber: 10,
+	})
+	if err != nil {
+		log.Fatalf("could not sum: %v\n", err)
+	}
+
+	log.Printf("Sum: %d\n", res.Result)
+}
